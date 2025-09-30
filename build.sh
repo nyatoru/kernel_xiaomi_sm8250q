3e193281f84f656e927498f1af1edf1b1d04dbec
Nyaru Toru <212608942+nyatoru@users.noreply.github.com>
2025-09-30 19:48:12 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -317,7 +317,7 @@ SET_CONFIG(){
             -e KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT \
             -e KSU_SUSFS_SUS_KSTAT \
             -e KSU_SUSFS_SPOOF_UNAME \
-            -e KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG \
+            -e KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
      else
         scripts/config --file out/.config \
             -d KSU_SUSFS \
